6efebfdf28b0c75fa3e386992b15c23b1fbfa30d
Loki <hungnguyen2003k@gmail.com>
2024-12-22 12:37:53 +0700

    Update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEp0PGZCf0cRHKvHX_3q00dCFUBotrRG4",
-  authDomain: "quan-ly-do-an-d3be6.firebaseapp.com",
-  projectId: "quan-ly-do-an-d3be6",
-  storageBucket: "quan-ly-do-an-d3be6.appspot.com",
-  messagingSenderId: "412206916441",
-  appId: "1:412206916441:web:ab41f52aee2007267ee248",
-  measurementId: "G-QPV13KRKRF",
+  apiKey: "...",
+  authDomain: "....",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "...",
 };
 
 // Initialize Firebase
